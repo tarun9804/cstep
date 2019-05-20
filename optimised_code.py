aa623776ce09a14769a9a5d215e670a9eef0e98e
tarun <tarun9804@gmail.com>
2019-05-20 14:52:42 +0530

    Plant_Sizing_Parameters added

--- optimised_code.py
+++ optimised_code.py
@@ -454,6 +454,78 @@ def Tcell_ResToModPower (Pgen_Hour_Status, Gt, Tamb_SH, WS_SH, a_CT, b_CT,
     return (Tcell, Mod_Isc, Mod_Voc, R_Pmod, Agg_R_Pmod)
 
 #----- End of cell temperature and resource to module power function ----------
+#----- End of cell temperature and resource to module power function ----------
+
+#------------------------------------------------------------------------------
+# Determination of plant sizing parameters
+#------------------------------------------------------------------------------
+#
+def Plant_Sizing_Parameters (V_PCU_Choice, Vuser, n, R_Pmod, Mod_Isc, Mod_Voc, \
+                             Pplant_Target, P_nomDC, P_PCU_AC, Vmp, Imp, Pmod, \
+                             V_maxDC, I_maxDC, SL_PC, EL_PC, Lmod, Bmod, \
+                             Vmppmax, Vmppmin):
+
+    if V_PCU_Choice == 1:
+        V_PCU_Ref = 0.5 * (Vmppmax + Vmppmin)
+    elif V_PCU_Choice == 2:
+        V_PCU_Ref = Vuser
+
+    Mod_Pmax = np.max(R_Pmod) * Pmod
+    Mod_Voc_min = np.min(Mod_Voc[np.nonzero(Mod_Voc)])
+    Mod_Voc_max = np.max(Mod_Voc[np.nonzero(Mod_Voc)])
+    Mod_Isc_max = np.max(Mod_Isc[np.nonzero(Mod_Voc)])
+    N_PCU = Pplant_Target *1000 // P_nomDC
+    m_max = V_maxDC // Mod_Voc_max
+    m = np.ceil(V_PCU_Ref / Vmp )
+    I_PCU_Ref = P_nomDC * 1000 / V_PCU_Ref
+    y_max = I_maxDC // (n * Mod_Isc_max)
+    y = np.ceil (I_PCU_Ref / (n * Imp))
+    N_mod_PCU = n * m * y
+    m_change = 0
+    DC_Losses = (1 - SL_PC)
+    P_PCU_DC_Max = N_mod_PCU * Mod_Pmax * DC_Losses * 0.001# 1000
+
+#    m_Pmod_max = Mod_Pmax * m/1000
+#    P_PCU_DC_Max = 230
+
+    Original_P_plant = N_mod_PCU * N_PCU * Pmod * 1e-06 #/ 1000000
+
+    if (m > m_max) or (y > y_max):
+        print('Invalid choice for PCU reference voltage')
+
+    if (P_PCU_DC_Max > P_nomDC):
+        while(P_PCU_DC_Max > P_nomDC):
+            N_mod_PCU_new = N_mod_PCU - m
+            m_change = m_change - 1
+            P_PCU_DC_Max = N_mod_PCU_new * Mod_Pmax * DC_Losses*0.001 #/1000
+            N_mod_PCU = N_mod_PCU_new
+            y = N_mod_PCU / (n * m)
+
+    else:
+        while (P_PCU_DC_Max <= P_nomDC):
+            N_mod_PCU_new =  N_mod_PCU + m
+            m_change = m_change + 1
+            P_PCU_DC_Max = N_mod_PCU_new * Mod_Pmax * DC_Losses *0.001 #/1000
+            N_mod_PCU = N_mod_PCU_new
+            y = N_mod_PCU / (n * m)
+
+    Pmax_Deviation_NewCap =  (P_PCU_DC_Max/P_nomDC) - 1
+    y_area = np.ceil (y)
+
+    N_Plant = N_mod_PCU * N_PCU
+    P_plant = N_Plant * Pmod * 1e-06#/ 1000000
+    Pure_mod_area = N_Plant * Lmod * Bmod
+    Pure_mod_area_acres = Pure_mod_area * 0.00024710
+    DC_Scaling_Factor = N_mod_PCU * Pmod / (1000 * P_PCU_AC)
+    P_plant_PCU = N_PCU * P_PCU_AC * 0.001#/ 1000
+
+
+    return (Mod_Pmax, Mod_Voc_min, Mod_Voc_max, Mod_Isc_max, N_PCU, m, \
+            m_change, N_mod_PCU, y, y_area, N_Plant, Pure_mod_area, \
+            Pure_mod_area_acres, DC_Scaling_Factor, P_plant, P_plant_PCU, \
+            Original_P_plant, Pmax_Deviation_NewCap, P_PCU_DC_Max)
+
+#-------- End of plant sizing parameters function -----------------------------
 
 
 ui.User_Assumed_Inputs()
@@ -526,5 +598,31 @@ ModTCell, ModIsc, ModVoc, RPmod, AggRPmod = \
                          ui.User_Assumed_Inputs.Module_Kt_Voc, \
                          ui.User_Assumed_Inputs.Module_Isc, \
                          ui.User_Assumed_Inputs.Module_Voc)
+ts2=time.time_ns()
+print(ts2-ts1)
+
+# Calling function that estimates the plant sizing parameters
+# This function optimises the plant design to generate maximum power for the
+# available resource
+ts1=time.time_ns()
+ModPmax, ModVocMin, ModVocMax, ModIscMax, N_PCU, m, m_change, NmodPCU, y, \
+y_area, NPlant, PureModArea, PureModAreaAcres, DCScalingFactor, Pplant, \
+PplantPCU, OriginalPplant, PmaxDeviationNewCap, P_PCU_DCMax = \
+    Plant_Sizing_Parameters (ui.User_Assumed_Inputs.V_PCU_Choice, \
+                         ui.User_Assumed_Inputs.Vuser, n, RPmod, ModIsc, ModVoc, \
+                         ui.User_Assumed_Inputs.Pplant_Target, \
+                         ui.User_Assumed_Inputs.PCU_PnomDC, \
+                         ui.User_Assumed_Inputs.PCU_P_PCU_AC, \
+                         ui.User_Assumed_Inputs.Module_Vmp, \
+                         ui.User_Assumed_Inputs.Module_Imp, \
+                         ui.User_Assumed_Inputs.Module_Pmod, \
+                         ui.User_Assumed_Inputs.PCU_VmaxDC, \
+                         ui.User_Assumed_Inputs.PCU_ImaxDC, \
+                         ui.User_Assumed_Inputs.Misc_SL_PC, \
+                         ui.User_Assumed_Inputs.Misc_EL_PC, \
+                         ui.User_Assumed_Inputs.Module_Lmod, \
+                         ui.User_Assumed_Inputs.Module_Bmod, \
+                         ui.User_Assumed_Inputs.PCU_Vmppmax, \
+                         ui.User_Assumed_Inputs.PCU_Vmppmin)
 ts2=time.time_ns()
 print(ts2-ts1)
